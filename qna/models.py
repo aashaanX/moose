1899df95a447ec60a529df6cfcf1b3fc6c205336
Arjun-ep <arjun@virtual.clinic>
2018-02-14 14:40:35 +0530

    qna module for question and answers

---
+++ qna/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from randomslugfield.fields import RandomSlugField
+
+from moose_user.models import MooseUser
+
+class Comment(models.Model):
+
+    comment_id = models.AutoField(primary_key=True)
+    comment_slug = RandomSlugField(length=9)
+    moose_user = models.ForeignKey(MooseUser, on_delete=models.CASCADE)
+    comment_description = models.TextField()
+
+    def __str__(self):
+        return self.comment_slug
+
+
+class Answer(models.Model):
+
+    answer_id = models.AutoField(primary_key=True)
+    answer_slug = RandomSlugField(length=9)
+    moose_user = models.ForeignKey(MooseUser,on_delete=models.CASCADE)
+    answer_description = models.TextField()
+    comments = models.ManyToManyField(Comment)
+    votes = models.IntegerField()
+
+    def __str__(self):
+        return self.answer_slug
+
+
+
+class Question(models.Model):
+    STATUS = (
+        ('open', 'open'),
+        ('closed', 'closed'),
+        ('wrong', 'wrong')
+    )
+
+    question_id = models.AutoField(primary_key=True)
+    question_slug = RandomSlugField(length=9)
+    moose_user = models.ForeignKey(MooseUser, on_delete=models.CASCADE)
+    question_title = models.CharField(max_length=500)
+    question_description = models.TextField()
+    question_status = models.CharField(max_length=20, choices=STATUS)
+    comments = models.ManyToManyField(Comment)
+    answers = models.ManyToManyField(Answer)
+
+    def __str__(self):
+        return self.question_title + "|" + self.moose_user.full_name
+
