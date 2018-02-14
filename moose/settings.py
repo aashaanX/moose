832f69918e1afed1960bcb957bb8716db99d36b2
Arjun-ep <arjun@virtual.clinic>
2018-02-14 15:44:23 +0530

    qna module for question and answers | model methods and regiseter for admn page

--- moose/settings.py
+++ moose/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'moose_user.apps.MooseUserConfig',
-    'rest_framework'
+    'rest_framework',
+    'qna.apps.QnaConfig',
 ]
 
 MIDDLEWARE = [
--- qna/models.py
+++ qna/models.py
@@ -14,6 +14,7 @@ class Comment(models.Model):
         return self.comment_slug
 
 
+
 class Answer(models.Model):
 
     answer_id = models.AutoField(primary_key=True)
@@ -21,7 +22,7 @@ class Answer(models.Model):
     moose_user = models.ForeignKey(MooseUser,on_delete=models.CASCADE)
     answer_description = models.TextField()
     comments = models.ManyToManyField(Comment)
-    votes = models.IntegerField()
+    votes = models.IntegerField(default=0, null= False)
 
     def __str__(self):
         return self.answer_slug
@@ -39,11 +40,19 @@ class Question(models.Model):
     question_slug = RandomSlugField(length=9)
     moose_user = models.ForeignKey(MooseUser, on_delete=models.CASCADE)
     question_title = models.CharField(max_length=500)
-    question_description = models.TextField()
-    question_status = models.CharField(max_length=20, choices=STATUS)
+    question_description = models.TextField(null=False)
+    question_status = models.CharField(max_length=20, choices=STATUS, default='open')
     comments = models.ManyToManyField(Comment)
     answers = models.ManyToManyField(Answer)
 
     def __str__(self):
         return self.question_title + "|" + self.moose_user.full_name
+
+    def get_answers(self):
+        answers_set =  self.answers.order_by('votes')
+        return answers_set.values('moose_user__full_name','answer_description', 'votes')
+
+    def get_comments(self):
+        return self.comments.values('comment_description', 'moose_user__full_name')
+
 
