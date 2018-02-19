54a6c1b6925604c00dfe17dc0cdc6a7391cd8321
aashaanX <arjunprahladan@gmail.com>
2018-02-20 01:52:41 +0530

    Complete API for adding question_comments

--- qna/urls.py
+++ qna/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('add_question/', views.AddQuestion.as_view()),
     path('retrive_question/', views.RetriveQuestion.as_view()),
     path('add_answer/', views.AddAnswer.as_view()),
+    path('add_question_comment/', views.AddQuestionComment.as_view()),
 ]
