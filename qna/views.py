18a97b9709b38cda0deef2640172fccfd89638ef
aashaanX <arjunprahladan@gmail.com>
2018-02-19 23:20:18 +0530

    Retrive Question with question_slug api completion

--- qna/views.py
+++ qna/views.py
@@ -8,42 +8,75 @@ from qna.serializers import AddQuestionSerializer, RetriveQuestionSerializer, Re
 
 
 class AddQuestion(APIView):
+    def post(self, request, format=None):
+        try:
+            question_data = AddQuestionSerializer(data=request.data)
+            if question_data.is_valid():
+                question_title = question_data.validated_data["question_title"]
+                question_description = question_data.validated_data["question_description"]
+                question = Question()
+                try:
+                    question.moose_user = request.user
+                except:
+                    return Response(data={"SUCCESS": False, "msg": "User not available"},
+                                    status=status.HTTP_401_UNAUTHORIZED)
+                question.question_title = question_title
+                question.question_description = question_description
+                try:
+                    question.save()
+                except:
+                    return Response(data={"SUCCESS": False, "msg": "Couldn't save question"},
+                                    status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+                return Response(data={"SUCCESS": True, "msg": "Question Added"},
+                                status=status.HTTP_200_OK)
+            else:
+                return Response(data={"SUCCESS": False, "msg": "request params missing or wrong"},
+                                status=status.HTTP_400_BAD_REQUEST)
+        except:
+            return Response(data={"SUCCESS": False, "msg": "Something went wrong"},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+
+class AddAnswer(APIView):
 
     def post(self, request, format=None):
-        question_data = AddQuestionSerializer(data=request.data)
-        if question_data.is_valid():
-            question_title = question_data.validated_data["question_title"]
-            question_description = question_data.validated_data["question_description"]
-            question = Question()
-            try:
-                question.moose_user = request.user
-            except:
-                return Response(data={"SUCCESS": False, "msg": "User not available"},
-                                status=status.HTTP_401_UNAUTHORIZED)
-            question.question_title = question_title
-            question.question_description = question_description
-            try:
-                question.save()
-            except:
-                return Response(data={"SUCCESS": False, "msg": "UnKnown"},
-                                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-            return Response(data={"SUCCESS": True, "msg": "Question Added"},
-                            status=status.HTTP_200_OK)
-        else:
-            return Response(data={"SUCCESS": False, "msg": "request params missing or wrong"},
-                            status=status.HTTP_400_BAD_REQUEST)
+        pass
 
 
-class  RetriveQuestion(APIView):
+
+class AddQuestionComment(APIView):
 
     def post(self, request, format=None):
-        request_data = RetriveQuestionSerializer(data=request.data)
-        if request_data.is_valid():
-            question_slug = request_data.validated_data["question_slug"]
-            qusetion_data = Question.objects.get(question_slug=question_slug)
-            question = RetriveQuestionOutputSerializer(qusetion_data)
-            return Response(data={'SUCCESS':True, 'question':question.data}, status=status.HTTP_200_OK)
-        else:
-            return Response(data={"SUCCESS": False, "msg": "request params missing or wrong"},
-                            status=status.HTTP_400_BAD_REQUEST)
+        pass
 
+
+class AddAnswerComment(APIView):
+
+    def post(self, request, format=None):\
+        pass
+
+
+
+
+class RetriveQuestion(APIView):
+    def post(self, request, format=None):
+        try:
+            request_data = RetriveQuestionSerializer(data=request.data)
+            if request_data.is_valid():
+                question_slug = request_data.validated_data["question_slug"]
+                qusetion_data = Question.objects.get(question_slug=question_slug)
+                question = RetriveQuestionOutputSerializer(qusetion_data)
+                return Response(data={'SUCCESS': True, 'question': question.data}, status=status.HTTP_200_OK)
+            else:
+                return Response(data={"SUCCESS": False, "msg": "request params missing or wrong"},
+                                status=status.HTTP_400_BAD_REQUEST)
+        except:
+            return Response(data={"SUCCESS": False, "msg":"Something went Wrong"})
+
+
+
+class VoteAnswer(APIView):
+
+    def post(self, request, format=None):
+        pass
